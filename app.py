a12109c44d5ed2a88d688d219b02f953b39db7ed
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-16 14:29:58 +0200

    feat: Add prompt to generate the rest of the script

--- app.py
+++ app.py
@@ -15,9 +15,15 @@ prompt_template = PromptTemplate(
         template='Please write me a prompt which will allow me to then create a P5.js script of {topic} without preload items and by using all the capabilities of P5.js, there should be no comments in the script and it should be as consice as possible'
         )
 
+
 script_template = PromptTemplate(
         input_variables= ['gen_prompt'],
         template='{gen_prompt}'
+        )
+
+completer_template = PromptTemplate(
+        input_variables= ['script'],
+        template=' if the script is incomplete please generate the rest of it, if it is complete return a whitespace. SCRIPT: {script}'
         )
 
 
@@ -25,10 +31,13 @@ script_template = PromptTemplate(
 llm = OpenAI(temperature=0.9)
 prompt_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True, output_key='gen_prompt')
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
-sequential_chain = SequentialChain(chains=[prompt_chain, script_chain], input_variables=['topic'], output_variables=['gen_prompt','script'], verbose=True)
+completer_chain = LLMChain(llm=llm, prompt=completer_template, verbose=True, output_key='script_completer')
+sequential_chain = SequentialChain(chains=[prompt_chain, script_chain, completer_chain], input_variables=['topic'], output_variables=['gen_prompt','script', 'script_completer'], verbose=True)
+
 
 
 if prompt:
     response = sequential_chain({'topic':prompt})
     st.write(response['gen_prompt'])
     st.write(response['script'])
+    st.write(response['script_completer'])
