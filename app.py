9c16ecadae87d1d4b2131d8a635c4adf05a68d45
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-16 13:52:55 +0200

    chore: Create basic streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import streamlit as st
 from apikey import apikey
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-print(apikey)
+st.title("P5 script generator")
+prompt = st.text_input("what do you want the script to draw?")
+    
