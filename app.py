d95110f38aadf954cd798a6f20fa881e45c9df34
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-16 14:36:19 +0200

    feat: Add comments to code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Imports
 import os
 import streamlit as st
 from apikey import apikey
@@ -5,11 +6,14 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 
+# ENV setup
 os.environ['OPENAI_API_KEY'] = apikey
 
+# Streamlit app
 st.title("P5 script generator")
 prompt = st.text_input("what do you want the script to draw?")
 
+# Prompt Templates to pass to LLM
 prompt_template = PromptTemplate(
         input_variables= ['topic'],
         template='Please write me a prompt which will allow me to then create a P5.js script of {topic} without preload items and by using all the capabilities of P5.js, there should be no comments in the script and it should be as consice as possible'
@@ -27,17 +31,22 @@ completer_template = PromptTemplate(
         )
 
 
-
+# LLM to use
 llm = OpenAI(temperature=0.9)
+
+# Chain components
 prompt_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True, output_key='gen_prompt')
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
 completer_chain = LLMChain(llm=llm, prompt=completer_template, verbose=True, output_key='script_completer')
+
+# Step to step process of chain
 sequential_chain = SequentialChain(chains=[prompt_chain, script_chain, completer_chain], input_variables=['topic'], output_variables=['gen_prompt','script', 'script_completer'], verbose=True)
 
-
-
+# Run when the user presses enter
 if prompt:
+    #Run prompt chain and add outputs into response variable
     response = sequential_chain({'topic':prompt})
+    # Output the responses to the streamlit application
     st.write(response['gen_prompt'])
     st.write(response['script'])
     st.write(response['script_completer'])
