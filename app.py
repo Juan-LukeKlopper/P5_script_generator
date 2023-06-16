c09cf8e35886a81f4a0661a0bef3f8e3e764a433
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-16 13:59:21 +0200

    feat: Add LLM and pass prompt to it

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
 import streamlit as st
 from apikey import apikey
+from langchain.llms import OpenAI
 
 os.environ['OPENAI_API_KEY'] = apikey
 
 st.title("P5 script generator")
 prompt = st.text_input("what do you want the script to draw?")
-    
+
+llm = OpenAI(temperature=0.9)
+
+if prompt:
+    response = llm(prompt)
+    st.write(response)
